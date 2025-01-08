f37efc48d3ac81e79ed130dc55608b4a691912ca
David Krcek <david@rotecodefraktion.de>
2025-01-08 20:19:12 +0100

    Initial Commit

---
+++ FirebaseBootcamp/FirebaseBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseBootcampApp.swift
+//  FirebaseBootcamp
+//
+//  Created by David Krcek on 08.01.25.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FirebaseBootcampTests/FirebaseBootcampTests.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseBootcampTests.swift
+//  FirebaseBootcampTests
+//
+//  Created by David Krcek on 08.01.25.
+//
+
+import Testing
+@testable import FirebaseBootcamp
+
+struct FirebaseBootcampTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
